417db9f2273d5dc3f91bfed2c924275c32669ee0
Canciu Costin <costin.canciu@ibm.com>
2020-04-12 01:09:57 +0300

    added dashboard page

--- app/admin/remote.rb
+++ app/admin/remote.rb
@@ -7,7 +7,7 @@ ActiveAdmin.register_page "Remote" do
   
   
       #render partial: "switch"
-      render partial : "smartphones/dropdown_smartphones", smartphones: Smartphone.all, smartphone: Smartphone.first
+      #render partial : "smartphones/dropdown_smartphones", smartphones: Smartphone.all, smartphone: Smartphone.first
   
       if params[:selected_smartphone_id].present?
           #render template: "smartphones/dropdown_list_smartphones", locals: {smartphones: Smartphone.all, smartphone: Smartphone.find(params[:selected_smartphone_id])}
@@ -18,12 +18,12 @@ ActiveAdmin.register_page "Remote" do
       end
       
   
-    #    div class: "blank_slate_container", id: "dashboard_default_message" do
-    #     span class: "blank_slate" do
-    #       span I18n.t("active_admin.dashboard_welcome.welcome")
-    #       small I18n.t("active_admin.dashboard_welcome.call_to_action")
-    #     end
-    #    end  
+       div class: "blank_slate_container", id: "dashboard_default_message" do
+        span class: "blank_slate" do
+          span I18n.t("active_admin.dashboard_welcome.welcome")
+          small I18n.t("active_admin.dashboard_welcome.call_to_action")
+        end
+       end  
   
     end # content
   end
