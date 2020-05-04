16b9e2a6e7e4835032032235712ae7607c3b97db
Costin <costin.canciu@gmail.com>
2020-05-04 22:47:56 +0300

    added terminal widget

--- app/admin/remote.rb
+++ app/admin/remote.rb
@@ -26,12 +26,12 @@ ActiveAdmin.register_page "Remote" do
       end
       
   
-       div class: "blank_slate_container", id: "dashboard_default_message" do
-        span class: "blank_slate" do
-          span I18n.t("active_admin.dashboard_welcome.welcome")
-          small I18n.t("active_admin.dashboard_welcome.call_to_action")
-        end
-       end  
+    #    div class: "blank_slate_container", id: "dashboard_default_message" do
+    #     span class: "blank_slate" do
+    #       span I18n.t("active_admin.dashboard_welcome.welcome")
+    #       small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    #     end
+    #    end  
   
     end # content
   end
